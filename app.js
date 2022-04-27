0de6ca66f1fc5ebf62ba670a365228c95d88a416
RustamTahmaz <103441069+RustamTahmaz@users.noreply.github.com>
2022-04-27 20:09:21 +0400

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-var cursor = document.getElementById('cursor')
-
-document.addEventListener('mousemove', function(e) {
-    var x = e.pageX
-    var y = e.pageY
-    cursor.style.left = x + "px"
-    cursor.style.top = y + "px"
-
-})
